2c60d18bf34a04dd4faaaf472c2eb483bdb405e5
David Garra <david.garra@carol.health>
2024-07-07 10:45:04 +0200

    Improve activity a9

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -10,20 +10,24 @@
 // * Use an if..else if..else block to determine what to print
 
 fn main() {
-    print_y(get_coordinate(1, 1));
-    print_y(get_coordinate(1, 10));
-    print_y(get_coordinate(1, 5));
-    print_y(get_coordinate(2, 1));
+    let (_, y) = get_coordinate(1, 1);
+    print_value(y);
+    let (_, y) = get_coordinate(1, 10);
+    print_value(y);
+    let (_, y) = get_coordinate(1, 5);
+    print_value(y);
+    let (_, y) = get_coordinate(2, 1);
+    print_value(y);
 }
 
 fn get_coordinate(x: i32, y: i32) -> (i32, i32) {
     (x, y)
 }
 
-fn print_y(coordinate: (i32, i32)) {
-    if coordinate.1 > 5 {
+fn print_value(value: i32) {
+    if value > 5 {
         println!(">5");
-    } else if coordinate.1 < 5 {
+    } else if value < 5 {
         println!("<5");
     } else {
         println!("=5");
