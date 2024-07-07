f5ec3200606d9df430e7c356a5091497427d6cb2
David Garra <david.garra@carol.health>
2024-07-07 10:43:01 +0200

    Complete activity a9

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -9,4 +9,23 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn main() {
+    print_y(get_coordinate(1, 1));
+    print_y(get_coordinate(1, 10));
+    print_y(get_coordinate(1, 5));
+    print_y(get_coordinate(2, 1));
+}
+
+fn get_coordinate(x: i32, y: i32) -> (i32, i32) {
+    (x, y)
+}
+
+fn print_y(coordinate: (i32, i32)) {
+    if coordinate.1 > 5 {
+        println!(">5");
+    } else if coordinate.1 < 5 {
+        println!("<5");
+    } else {
+        println!("=5");
+    }
+}
