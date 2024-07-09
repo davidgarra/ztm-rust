4e46b9ef834e6189556701ec7219233976acd2f4
David Garra <david.garra@carol.health>
2024-07-09 17:56:17 +0200

    Complete activity a13

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -10,4 +10,13 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    let my_vec = vec![10, 20, 30, 40];
+    for i in &my_vec {
+        match i {
+            30 => println!("thirty"),
+            _ => println!("{}", i),
+        }
+    }
+    println!("Lenght: {}", my_vec.len());
+}
