1791e47fd7fbed72961c7e2d0a842eb58c673383
David Garra <david.garra@carol.health>
2024-07-09 17:56:24 +0200

    Complete activity a14

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,35 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+#[derive(Debug)]
+struct Person {
+    age: i32,
+    name: String,
+    color: String,
+}
+
+impl Person {
+    fn new(name: String, age: i32, color: String) -> Self {
+        Person { name, age, color }
+    }
+}
+
+fn print(value: &str) {
+    println!("{}", value)
+}
+
+fn main() {
+    let people = vec![
+        Person::new(String::from("David"), 9, String::from("red")),
+        Person::new(String::from("Pamela"), 15, String::from("blue")),
+        Person::new(String::from("Sara"), 8, String::from("green")),
+    ];
+
+    for person in people {
+        if person.age <= 10 {
+            print(&person.name);
+            print(&person.color);
+            println!("{:?}\n", person);
+        }
+    }
+}
