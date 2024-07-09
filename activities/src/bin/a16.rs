41d135323dd4a87b10e2a1e62c9ac20b593c0e15
David Garra <david.garra@carol.health>
2024-07-09 18:52:51 +0200

    Complete activity a16

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -8,4 +8,29 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
+
+impl Student {
+    fn print_locker(&self) {
+        match self.locker {
+            Some(n) => println!("{} locker: {}", self.name, n),
+            None => println!("No locker assigned to {}", self.name),
+        }
+    }
+}
+
+fn main() {
+    let david = Student {
+        name: "David".to_owned(),
+        locker: Some(10),
+    };
+    let vanessa = Student {
+        name: "Vanessa".to_owned(),
+        locker: None,
+    };
+    david.print_locker();
+    vanessa.print_locker();
+}
