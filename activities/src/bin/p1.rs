ae4553041777528ac8de66f7b06a6bcea0853521
David Garra <david.garra@carol.health>
2024-07-11 19:43:17 +0200

    Complete activity p1:stage1

--- activities/src/bin/p1.rs
+++ activities/src/bin/p1.rs
@@ -29,4 +29,88 @@
 // * A vector is the easiest way to store the bills at stage 1, but a
 //   hashmap will be easier to work with at stages 2 and 3.
 
-fn main() {}
+use std::io::stdin;
+
+mod menu {
+    pub enum Item {
+        AddBill = 1,
+        ShowBills,
+    }
+    impl Item {
+        pub fn from_str(value: &str) -> Option<Self> {
+            match value {
+                "1" => Some(Item::AddBill),
+                "2" => Some(Item::ShowBills),
+                _ => None,
+            }
+        }
+    }
+
+    pub fn show() {
+        println!("\n--- Interactive Bill Manager ---\n");
+        println!("Make a choice:");
+        println!("1) Add a bill");
+        println!("2) Show bills");
+    }
+}
+
+#[derive(Debug)]
+struct Bill {
+    name: String,
+    amount: f64,
+}
+
+fn read_line() -> String {
+    let mut buf = String::new();
+    while stdin().read_line(&mut buf).is_err() {
+        println!("Please, try again");
+    }
+    buf.trim().to_owned()
+}
+
+fn read_amount() -> f64 {
+    loop {
+        let Ok(value) = read_line().parse::<f64>() else {
+            continue;
+        };
+        return value;
+    }
+}
+
+fn add_bill(bills: &mut Vec<Bill>) {
+    println!("Insert name: ");
+    let name = read_line();
+    println!("Insert amount: ");
+    let amount = read_amount();
+    let bill = Bill { name, amount };
+    println!("Adding {:?} to the bills", bill);
+    bills.push(bill);
+}
+
+fn show_bills(bills: &mut Vec<Bill>) {
+    println!("List of bills({}):", bills.len());
+    for item in bills {
+        println!("{:?}", item);
+    }
+}
+
+fn run() {
+    let mut bills: Vec<Bill> = vec![];
+    loop {
+        use menu::Item;
+        menu::show();
+        let input = read_line();
+        let Some(choice) = Item::from_str(input.as_str()) else {
+            println!("Unknown command");
+            continue;
+        };
+        match choice {
+            Item::AddBill => add_bill(&mut bills),
+            Item::ShowBills => show_bills(&mut bills),
+        }
+    }
+}
+
+fn main() {
+    run();
+}
