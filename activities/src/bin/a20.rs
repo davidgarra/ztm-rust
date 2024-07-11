890ba278b43650121ffbbae17c2edc15b95fd900
David Garra <david.garra@carol.health>
2024-07-11 15:07:44 +0200

    Complete activity a20

--- activities/src/bin/a20.rs
+++ activities/src/bin/a20.rs
@@ -23,4 +23,60 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+use std::io::{self, stdin};
+
+enum PowerState {
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate,
+}
+
+impl PowerState {
+    fn new(power_state: &str) -> Result<Self, &str> {
+        use PowerState::*;
+        match power_state.to_lowercase().as_str() {
+            "off" => Ok(Off),
+            "sleep" => Ok(Sleep),
+            "reboot" => Ok(Reboot),
+            "shutdown" => Ok(Shutdown),
+            "hibernate" => Ok(Hibernate),
+            _ => Err("Unknown state"),
+        }
+    }
+    fn print(&self) {
+        use PowerState::*;
+        match self {
+            Off => println!("turning off"),
+            Sleep => println!("sleeping"),
+            Reboot => println!("rebooting"),
+            Shutdown => println!("shutting down"),
+            Hibernate => println!("hiberneting"),
+        }
+    }
+}
+
+fn input_line() -> io::Result<String> {
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer)?;
+    Ok(buffer.trim().to_owned())
+}
+
+fn print_state(state: &str) {
+    let power_state = PowerState::new(state);
+    match power_state {
+        Ok(power_state) => power_state.print(),
+        Err(error) => println!("{error}"),
+    }
+}
+
+fn main() {
+    loop {
+        let line = input_line();
+        match line {
+            Err(error) => println!("{error}"),
+            Ok(input) => print_state(&input),
+        }
+    }
+}
